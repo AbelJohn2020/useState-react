a1864691db8ee889fe734627949b3b1f22d9e6a2
Abel AR <abel_john_14@hotmail.com>
2021-02-16 22:16:45 -0500

    print boolean as a string

--- usestate-app/src/ConditionalApp.js
+++ usestate-app/src/ConditionalApp.js
@@ -9,16 +9,17 @@ const ConditionalApp = () => {
             <button onClick={() => setCondition(!condition)}>
                 Toggle
             </button>
+
+            <h1>State value is {condition.toString()} </h1>
+
             {
                 // // this is a conditional but is exclusive in true
                 condition &&
                 // ? <h1>Show message in True</h1>
                 // : <h1>Another message in false</h1>
-                <h1>Show message in True</h1>
+                <h1>Show message in true</h1>
                 // : null
             }
-
-            <h1>State value is {condition} </h1>
         </div>
     )
 }
